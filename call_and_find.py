7bc7a05d437d6bd83da4931a1b952e0f1f5969ad
MAZBELAZ <PAVELTESLIN@yandex.ru>
2018-01-26 01:10:40 +0300

    1

--- call_and_find.py
+++ call_and_find.py
@@ -1,12 +1,19 @@
+w="11.01.2018"
 file=open("text.txt","r")
-if
+text=file.read()
 file.read(10)
 file.close()
+c = text.count(w)
+while c > 0:
+    print(w)
+    c -= 1
 #print(cont)
 #print (file.read(4))
 #file.close()
 # print(2)
 
-#my_file = open("some.txt", "w")
-#my_file.write("Мне нравится Python!Это классный язык!")
-#my_file.close()
+my_file = open("some.txt", "w")
+my_file.write("text.txt")
+my_file.close()
+
+
