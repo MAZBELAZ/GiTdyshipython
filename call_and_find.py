0c54e5164f21c21637123207b37293e93f1d90e0
MAZBELAZ <PAVELTESLIN@yandex.ru>
2018-02-04 23:36:04 +0300

    1d

--- call_and_find.py
+++ call_and_find.py
@@ -2,10 +2,11 @@ w="11.01.2018"
 file=open("text.txt","r")
 text=file.read()
 file.read(10)
+print(text)
 file.close()
 c = text.count(w)
 while c > 0:
-    print(w)
+    print(c)
     c -= 1
 
 #print(cont)
@@ -13,8 +14,8 @@ while c > 0:
 #file.close()
 # print(2)
 
-my_file = open("some.txt", "w")
-my_file.write("text.txt")
-my_file.close()
+#my_file = open("some.txt", "w")
+#my_file.write("text.txt")
+#my_file.close()
 
 
