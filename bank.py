6f28f4c05e53df0553fdcbdf79449148b2d4f0df
MAZBELAZ <PAVELTESLIN@yandex.ru>
2018-03-12 23:12:21 +0300

    s

--- bank.py
+++ bank.py
@@ -1,12 +1,13 @@
-deposit=int(input("deposit="))
-percent=int(input(("percent="))
-day=int(input("day="))
-fullpercent=int(100)
-year=int(365)
+deposit = int(input("deposit="))
+percent = int(input("percent="))
+day = int(input("day="))
+fullpercent = 100
+year = 365
+tax = 0.13
 
-income=deposit*percent*day/fullpercent/year
-profit=income-tax*income
-total=deposit+profit
+income = deposit*percent*day/fullpercent/year
+profit = income-tax*income
+total = deposit+profit
 #popolnenie!!!!!!!!!!!!
 
 print(deposit)
